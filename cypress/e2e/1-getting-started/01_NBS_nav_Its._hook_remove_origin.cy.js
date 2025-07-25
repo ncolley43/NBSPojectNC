179d6c6d99c4f16cf68558bc7b79548955d74042
Nik Colley <col43@hotmail.com>
2025-07-25 11:02:20 +0100

    slight update dont use this file

--- cypress/e2e/1-getting-started/01_NBS_nav_Its._hook_remove_origin.cy.js
+++ cypress/e2e/1-getting-started/01_NBS_nav_Its._hook_remove_origin.cy.js
@@ -8,8 +8,8 @@ describe('Sams Homework', () => {
     // Accept cookies and search for Dyson (common setup for most tests)
     cy.contains('button', 'Accept All Cookies').click();
     cy.get('[data-cy="searchFieldSearch"]').first().type('Dyson');
-    cy.contains("Dyson", { timeout: 10000 })
-      .should('be.visible', { timeout: 10000 })
+    cy.contains("Dyson", { timeout: 40000 })
+      .should('be.visible', { timeout: 40000 })
       .click();
   });
 
@@ -18,39 +18,39 @@ describe('Sams Homework', () => {
   });
 
   it('2 - I verify the telephone link has the correct number, protocol and href', () => {
-    cy.contains('08003457788', { timeout: 10000 }).should('be.visible');
-    cy.get('a[title="Call 08003457788"]', { timeout: 10000 })
+    cy.contains('08003457788', { timeout: 40000 }).should('be.visible');
+    cy.get('a[title="Call 08003457788"]', { timeout: 40000 })
       .should('be.visible')
       .and('have.attr', 'href', 'tel:08003457788');
   });
 
   it('3 - I verify the h1 title text on page is as expected', () => {
-    cy.contains('h1', 'Dyson', { timeout: 10000 }).should('be.visible');
+    cy.contains('h1', 'Dyson', { timeout: 40000 }).should('be.visible');
   });
 
   it('4 - I verify the href attribute of the Source logo is as expected', () => {
     // Navigate back to main page first since this test doesn't need Dyson search
     cy.visit('https://source.thenbs.com');
-    cy.get('a.brand-primary.wrapper', { timeout: 10000 })
+    cy.get('a.brand-primary.wrapper', { timeout: 40000 })
       .should('be.visible')
       .and('have.attr', 'href', '/');
   });
 
   it('5 - I verify the external manufacturer link attribute contains the correct url', () => {
-    cy.get('a[title="Visit https://www.dyson.co.uk/commercial/overview/architects-designers"]', { timeout: 10000 })
+    cy.get('a[title="Visit https://www.dyson.co.uk/commercial/overview/architects-designers"]', { timeout: 40000 })
       .should('be.visible')
       .and('have.attr', 'href', 'https://www.dyson.co.uk/commercial/overview/architects-designers')
       .and('have.attr', 'target', '_blank');
   });
 
   it('6 - I verify the contact manufacturer button shows the correct text', () => {
-    cy.get('.mdc-button__label', { timeout: 10000 })
+    cy.get('.mdc-button__label', { timeout: 40000 })
       .should('be.visible')
       .and('contain', 'Contact manufacturer');
   });
 
   it('7 - I verify the Overview tab name and URL', () => {
-    cy.get('a[data-cy="overviewTab"]', { timeout: 10000 })
+    cy.get('a[data-cy="overviewTab"]', { timeout: 40000 })
       .should('be.visible')
       .and('have.attr', 'href', '/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview');
   });
@@ -61,8 +61,8 @@ describe('Sams Homework', () => {
     
     cy.contains('button', 'Accept All Cookies').click();
     cy.get('[data-cy="searchFieldSearch"]').first().type('Dyson');
-    cy.contains("Dyson", { timeout: 10000 })
-      .should('be.visible', { timeout: 10000 })
+    cy.contains("Dyson", { timeout: 40000 })
+      .should('be.visible', { timeout: 40000 })
       .click();
     
     const expectedTabs = [
